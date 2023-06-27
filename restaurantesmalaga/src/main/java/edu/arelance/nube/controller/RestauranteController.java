254c84f7a9c7f64cdde2f322dfb627445b4b53ce
Jorge <jorge.mihalyczo@hotmail.com>
2023-06-27 14:44:15 +0200

    Rest

--- restaurantesmalaga/src/main/java/edu/arelance/nube/controller/RestauranteController.java
+++ restaurantesmalaga/src/main/java/edu/arelance/nube/controller/RestauranteController.java
@@ -1,23 +1,18 @@
 package edu.arelance.nube.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.arelance.nube.repository.entity.Restaurante;
-
-/*
- * API WEB
- * HTTP -> Deriva en la ejecucion de un metodo
- * 
- * GET -> Consultar todos
- * GET -> Consultar uno (por ID)
- * POST -> Insertar restaurante nuevo
- * PUT -> Modificar un restaurante que ya existe 
- * DELETE -> Eliminar un restaurante (por ID)
- * GET -> Búsqueda -> Por barrio, por especialidad, precioMedio, etc.
- * */
+import edu.arelance.nube.service.RestauranteService;
 
 
 // @Controller // devuelve una vista
@@ -25,13 +20,56 @@ import edu.arelance.nube.repository.entity.Restaurante;
 @RequestMapping("/restaurante")
 public class RestauranteController {
 	
+	@Autowired
+	RestauranteService restauranteService;
+	
 	@GetMapping("/test") // http://localhost:8081/restaurante/test
 	public Restaurante obtenerRestauranteTest () {
 		Restaurante restaurante = new Restaurante();
 		
 		System.err.println("Llamando a obtener restauranteTest");
-		
+		restaurante = new Restaurante(1l, "Martinete", "Carlos Haya 33", "Carranque",
+				"www.martinete.org", "http://google.xe", 33.65f, -2.3f, 10, "gazpachuelo",
+				"Paella", "Sopa de Marizcos", LocalDateTime.now());
 		return restaurante;
 	}
+	
+	// GET -> Consultar todos http://localhost:8081/restaurante
+	@GetMapping
+	public ResponseEntity<?> listarTodos(){
+		ResponseEntity<?> responseEntity = null;
+		Iterable<Restaurante> lista_restaurantes = null;
+		
+		lista_restaurantes = this.restauranteService.consultarTodos();
+		responseEntity = ResponseEntity.ok(lista_restaurantes);
+		
+		return responseEntity; 
+	}
+	
 
+	
+	 // GET -> Consultar uno (por ID) http://localhost:8081/restaurante/id
+	@GetMapping("/{id}")
+	public ResponseEntity<?> listarPorId(@PathVariable Long id){
+		ResponseEntity<?> responseEntity = null;
+		
+		
+		
+		return responseEntity; 
+	}
+	 // POST -> Insertar restaurante nuevo http://localhost:8081/restaurante (Body, Restaurante)
+	 // PUT -> Modificar un restaurante que ya existe http://localhost:8081/restaurante (Body, Restaurante)
+	 // GET -> Búsqueda -> Por barrio, por especialidad, precioMedio, etc.
+	 
+
+	// DELETE -> Eliminar un restaurante (por ID) http://localhost:8081/restaurante/id
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> borrarPorId(@PathVariable Long id) {
+		ResponseEntity<?> responseEntity = null;
+		
+		
+		
+		return responseEntity; 
+	}
+	
 }
--- restaurantesmalaga/src/main/java/edu/arelance/nube/service/RestauranteServiceImpl.java
+++ restaurantesmalaga/src/main/java/edu/arelance/nube/service/RestauranteServiceImpl.java
@@ -1,8 +1,51 @@
 package edu.arelance.nube.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.arelance.nube.repository.RestauranteRepository;
+import edu.arelance.nube.repository.entity.Restaurante;
 
 @Service
 public class RestauranteServiceImpl implements RestauranteService {
+
+	
+	RestauranteRepository restauranteRepository;
+	
+	@Override
+	@Transactional(readOnly = true) // permitir acceso recurrente a la lectura de datos
+	public Iterable<Restaurante> consultarTodos() {
+		return this.restauranteRepository.findAll();
+		// return null;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Restaurante> consultarRestaurante(Long id) {
+		return this.restauranteRepository.findById(id);
+		// return Optional.empty();
+	}
+
+	@Override
+	@Transactional
+	public Restaurante altaRestaurante(Restaurante restaurante) {
+		return this.restauranteRepository.save(restaurante);
+		// return null;
+	}
+
+	@Override
+	@Transactional
+	public void borrarRestaurante(Long id) {
+		this.restauranteRepository.deleteById(id);
+	}
+
+	@Override
+	@Transactional
+	public Optional<Restaurante> modificarRestaurante(Long id, Restaurante restaurante) {
+		
+		return Optional.empty();
+	}
 
 }
